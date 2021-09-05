653e62c573a0231ed83b4e813ce0b6f6000ad099
Kun <yukun0620@gmail.com>
2021-09-05 22:31:49 +0800

    bomb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,146 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import Inputs from "./components/Inputs";
-import TodoItem from "./components/TodoItem";
-import EditArea from "./components/TodoItem";
+import styled from "styled-components";
+
+const EditBackground = styled.div`
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+`;
+
+const EditWrapper = styled.div`
+  background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  z-index: 10;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px solid black;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  margin-top: 40px;
+  justify-content: space-between;
+  width: 70%;
+`;
+
+
+
+const TodoItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  border: 1px solid black;
+
+  & + & {
+    margin-top: 12px;
+  }
+`;
+const TodoContent = styled.div`
+  color: rgba(2, 40, 77)
+  font-size: 16px;
+
+  ${(props) =>
+    props.$isDone &&
+    `
+    text-decoration: line-through;
+  `}
+`;
+const TodoButtonWrapper = styled.div``;
+const Button = styled.button`
+  padding: 4px;
+  color: black;
+
+  &:hover {
+    color: red;
+  }
+
+  & + & {
+    margin-left: 4px;
+  }
+`;
+
+
+function EditArea({
+  editedTodo,
+  toggleEditArea,
+  handleEditTodo,
+}) {
+  const [editingContent, setEditingContent] = useState(editedTodo.content);
+  const handleEditChange = (e) => {
+    setEditingContent(e.target.value);
+  };
+  return (
+    <EditBackground>
+      <EditWrapper>
+        <input value={editingContent} onChange={handleEditChange}></input>
+        <Buttons>
+          <button
+            onClick={() => {
+              handleEditTodo(editingContent, editedTodo.id);
+            }}
+          >
+            送出
+          </button>
+          <button onClick={toggleEditArea}>取消</button>
+        </Buttons>
+      </EditWrapper>
+    </EditBackground>
+  );
+}
+
+function TodoItem({
+  todo,
+  handleSetEditTodo,
+  handleDeleteTodo,
+  handleToggleTodo,
+  handleEditTodo,
+  toggleEditArea,
+}) {
+  return (
+    <TodoItemWrapper>
+      <TodoContent $isDone={todo.isDone}>{todo.content}</TodoContent>
+      <TodoButtonWrapper>
+        <Button
+          onClick={() => {
+            // handleSetEditTodo(todo);
+            toggleEditArea();
+          }}
+        >
+          編輯
+        </Button>
+        <Button
+          onClick={() => {
+            handleToggleTodo(todo.id);
+          }}
+        >
+          {todo.isDone ? "未完成" : "已完成"}
+        </Button>
+        <Button
+          onClick={() => {
+            handleDeleteTodo(todo.id);
+          }}
+        >
+          刪除
+        </Button>
+      </TodoButtonWrapper>
+    </TodoItemWrapper>
+  );
+}
 
 function App() {
   const id = useRef(1);
@@ -92,7 +231,7 @@ function App() {
 
   useEffect(() => {
     filterTodos();
-  }, [todos, status, filterTodos]);
+  }, [todos, status]);
 
   return (
     <div className="App">
